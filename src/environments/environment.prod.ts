248bf2660b2d7aad5a24958dcea59554ad25db7c
iamkosgei <kosgeik2930@gmail.com>
2019-04-06 19:10:27 +0300

    added firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDoJHCgzBUxQtCFn2qg6_vCUGadJri3NGw",
+    authDomain: "foodangular.firebaseapp.com",
+    databaseURL: "https://foodangular.firebaseio.com",
+    projectId: "foodangular",
+    storageBucket: "foodangular.appspot.com",
+    messagingSenderId: "193747555337"
+  }
 };
